da1a9771537a417b06e35f2b9dd6da8715efca32
Beth <bethmelmtv@gmail.com>
2022-07-25 17:14:03 -0700

    added components and downloaded node modules

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+import styles from './About.css';
+
+export default function About() {
+  return (
+    <div className={styles.About}>
+      <p>I am the About page</p>
+    </div>
+  );
+}
---
+++ src/components/List/List.jsx
@@ -0,0 +1,9 @@
+import styles from './List.css';
+
+export default function List() {
+  return (
+    <div className={styles.ListPage}>
+      <p>I am the List page</p>
+    </div>
+  );
+}
