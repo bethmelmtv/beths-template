491b5b351049ee66b59a28e5f5e3858c3b25498f
Beth <bethmelmtv@gmail.com>
2022-08-02 15:47:12 -0700

    added routes to app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,18 +8,30 @@ import Layout from './Page/Layout.jsx';
 import Home from './Home/Home.jsx';
 import About from './About/About.jsx';
 import Pokedex from './Pokedex/Pokedex.jsx';
+import FuzzyBunny from './FuzzyBunny/FuzzyBunny.jsx';
+import Families from './FuzzyBunny/Families.jsx';
+import Bunnies from './FuzzyBunny/Bunnies.jsx';
+import FuzzyBunnyProvider from '../state/context/FuzzyBunnyContext.jsx';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="pokedex" element={<Pokedex />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+      <FuzzyBunnyProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="pokedex" element={<Pokedex />} />
+            <Route path="about" element={<About />} />
+            <Route path="fuzzy-bunny" element={<FuzzyBunny />}>
+              <Route index element={<Families />} />
+              <Route path="bunnies" element={<Bunnies />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+            {/* what does
+          this line of code do? */}
+          </Route>
+        </Routes>
+      </FuzzyBunnyProvider>
     </Router>
   );
 }
