4912957defad992f36c4a61158587602f5fb7d35
Beth <bethmelmtv@gmail.com>
2022-08-12 09:36:30 -0700

    added sign up, sign in and signout to service

--- src/state/services/user-service.js
+++ src/state/services/user-service.js
@@ -4,6 +4,18 @@ export function getUser() {
   return client.auth.user();
 }
 
+export async function signUp(credentials) {
+  return await client.auth.signUp(credentials);
+}
+
+export async function signIn(credentials) {
+  return await client.auth.signIn(credentials);
+}
+
+export async function signOut() {
+  return await client.auth.signOut();
+}
+
 const PROFILE = 'profile';
 
 export function getLocalProfile() {
