4f01cd041599c11334cc456ec018d2afd0088431
Beth <bethmelmtv@gmail.com>
2022-08-12 09:49:15 -0700

    added userprovider to app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,26 +11,29 @@ import Pokedex from './Pokedex/Pokedex.jsx';
 import { Toaster } from 'react-hot-toast';
 import FamilyList from './Teas/FamilyList';
 import TeaProvider from '../state/context/TeaContext';
+import UserProvider from '../state/context/UserContext.jsx';
 import Families from '../components/Teas/Families';
 
 export default function App() {
   return (
     <Router>
-      <Toaster />
-      <TeaProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="pokedex" element={<Pokedex />} />
-            <Route path="about" element={<About />} />
-            <Route path="tea-list" element={<FamilyList />} />
-            <Route path="tea-families" element={<Families />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-            {/* what does
+      <UserProvider>
+        <Toaster />
+        <TeaProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="pokedex" element={<Pokedex />} />
+              <Route path="about" element={<About />} />
+              <Route path="tea-list" element={<FamilyList />} />
+              <Route path="tea-families" element={<Families />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+              {/* what does
           this line of code do? */}
-          </Route>
-        </Routes>
-      </TeaProvider>
+            </Route>
+          </Routes>
+        </TeaProvider>
+      </UserProvider>
     </Router>
   );
 }
